82d824a3dc586eb85bb4281676038ff5d80cb14e
Lars Hansen <lars.breum.hansen@live.dk>
2017-07-25 08:06:03 +0200

    weekly traffic line chart works

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//Graphs
+var ctx = document.getElementById("daily-traffic");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+        datasets: [{
+            label: 'Visitors',
+            data: [0, 500, 1250, 800, 1800, 1900, 2000, 2500, 2600, 2400, 2500],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
