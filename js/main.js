41a0b6f9aa3df2bfc9b7e848f064a2dc90255c50
Lars Hansen <lars.breum.hansen@live.dk>
2017-07-25 08:45:13 +0200

    added hourly, daily, and monthly graphs

--- js/main.js
+++ js/main.js
@@ -1,12 +1,128 @@
 //Graphs
-var ctx = document.getElementById("daily-traffic");
-var myChart = new Chart(ctx, {
+let hourlyTrafficGraph = document.getElementById("hourly-traffic")
+let weeklyTrafficGraph = document.getElementById("weekly-traffic");
+let dailyTrafficGraph = document.getElementById("daily-traffic")
+let monthlyTrafficGraph = document.getElementById("monthly-traffic");
+
+//Hourly
+let hourlyChart = new Chart(hourlyTrafficGraph, {
+    type: 'line',
+    data: {
+        labels: ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
+        datasets: [{
+            label: 'Visitors',
+            data: [0, 0, 0, 0, 10, 50, 50, 70, 20, 100, 20, 24, 11, 50, 11, 15, 40, 120, 10, 111, 102, 40, 23, 25, 11, 0],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+//Daily
+let dailyChart = new Chart(dailyTrafficGraph, {
+    type: 'line',
+    data: {
+        labels: ["M", "T", "W", "T", "F", "L", "S"],
+        datasets: [{
+            label: 'Visitors',
+            data: [0, 100, 150, 80, 300, 200, 20],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+//Weekly
+let weeklyChart = new Chart(weeklyTrafficGraph, {
     type: 'line',
     data: {
         labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
         datasets: [{
             label: 'Visitors',
             data: [0, 500, 1250, 800, 1800, 1900, 2000, 2500, 2600, 2400, 2500],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+//monthly
+let monthlyChart = new Chart(monthlyTrafficGraph, {
+    type: 'line',
+    data: {
+        labels: ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+        datasets: [{
+            label: 'Visitors',
+            data: [8000, 8330, 8440, 8320, 9490, 9944, 8300, 8500, 9340, 8284, 9944, 9934],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
